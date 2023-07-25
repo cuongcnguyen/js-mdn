0c6cb61b17dd416b27a04b1bc460522e1cf9cb65
cuong <cuongcao911@gmail.com>
2023-07-25 19:44:33 +0700

    Finished 3rd party api

---
+++ client-side-apis/3rd-party-apis/mapquest/script.js
@@ -0,0 +1,24 @@
+L.mapquest.key = 'MFpj01ChA26MTuODji3aG60FMqDr5O9l';
+
+// 'map' refers to a <div> element with the ID map
+const map = L.mapquest.map('map', {
+  center: [53.480759, -2.242631],
+  layers: L.mapquest.tileLayer('hybrid'),
+  zoom: 12
+});
+
+map.addControl(L.mapquest.control({ position: "bottomright" }));
+
+L.marker([53.480759, -2.242631], {
+  icon: L.mapquest.icons.marker({
+    primaryColor: "#22407F",
+    secondaryColor: "#3B5998",
+    shadow: true,
+    size: "md",
+    symbol: "A",
+  }),
+})
+  .bindPopup("This is Manchester!")
+  .addTo(map);
+
+
